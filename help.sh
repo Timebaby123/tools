36c29be744d95fcc775c36001210d984975d3bbb
Timebaby123 <84785972+Timebaby123@users.noreply.github.com>
2021-05-25 21:28:40 +0700

    Add files via upload

---
+++ help.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo HELP
+echo SCAN TOOLS
+echo python3 portscanner.py ip startport endport
+echo DOS/DDoS Tools
+echo TCP/UDP:python3 flood.py
+echo Slowloris:python3 slowloris.py url
+echo HTTP Flood:python3 pyflooder.py url port threads
